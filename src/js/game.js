9e8be0618a64866ae644a76fe55a7acd8bd908cd
Nikolay1990 <Nikolaicev1990@gmail.com>
2023-10-18 20:49:16 +0300

    initial commit

---
+++ src/js/game.js
@@ -0,0 +1,23 @@
+const character = require("./domain");
+
+class Game {
+  start() {
+    console.log('game started');
+  }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
+
+module.exports = {
+  Game,
+  GameSavingData,
+  readGameSaving,
+  writeGameSaving,
+}
